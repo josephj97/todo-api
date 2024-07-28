283c6f693e6e0e46485fc7994929a2670cad343a
Joseph Atef <josephatef.j97@gmail.com>
2024-07-28 22:57:47 +0300

    description is not required

--- models/todo.js
+++ models/todo.js
@@ -1,46 +1,46 @@
 const mongoose = require('mongoose');
 
 const todoSchema = mongoose.Schema({
-    title: {
-        type: String,
-        required: true
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: false,
+    default: 'N/A',
+  },
+  onDate: {
+    type: Date,
+    required: true,
+    default: Date.now,
+  },
+  cardColor: {
+    type: String,
+    required: true,
+    default: '#cddc39',
+  },
+  isCompleted: {
+    type: Boolean,
+    required: true,
+    default: false,
+  },
+  timestamps: {
+    createdOn: {
+      type: Date,
+      required: true,
+      default: Date.now,
     },
-    description: {
-        type: String,
-        required: true,
-        default: 'N/A'
+    modifiedOn: {
+      type: Date,
+      required: true,
+      default: Date.now,
     },
-    onDate: {
-        type: Date,
-        required: true,
-        default: Date.now
+    completedOn: {
+      type: Date,
+      default: null,
     },
-    cardColor: {
-        type: String,
-        required: true,
-        default: '#cddc39'
-    },
-    isCompleted: {
-        type: Boolean,
-        required: true,
-        default: false
-    },
-    timestamps: {
-        createdOn: {
-            type: Date,
-            required: true,
-            default: Date.now
-        },
-        modifiedOn: {
-            type: Date,
-            required: true,
-            default: Date.now
-        },
-        completedOn: {
-            type: Date,
-            default: null
-        }
-    }
+  },
 });
 
-module.exports = mongoose.model("Todo", todoSchema);
+module.exports = mongoose.model('Todo', todoSchema);
